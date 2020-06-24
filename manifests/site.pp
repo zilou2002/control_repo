106249292df16fa199e29860d4b2908411906b69
zilou2002 <67327812+zilou2002@users.noreply.github.com>
2020-06-24 13:55:19 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,5 @@
 node default {
 }
-
 node 'master.puppet.vm' {
   include role::master_server
   file { '/root/README':
@@ -8,11 +7,12 @@ node 'master.puppet.vm' {
   content => "Welcome to ${fqdn}\n",
   }
 }
-
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
+}
 node /^web/ {
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
