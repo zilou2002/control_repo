c331d5f944283c7c2eddc3e138b444f7bd362092
zilou2002 <67327812+zilou2002@users.noreply.github.com>
2020-06-24 11:39:20 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqgsDlZslzrumE7DMLvYc7BVYapRo2ZJNxiwYmG+tXmRpQGND7NK+zfGtzq9O8KQMjGQNn1Dj2J6hKFqrcjxZy2fcAlt4JYUGU9Omm3ielAzg0KVMWgfjxzAI6OsXqqhyDI8L0/kDc/CiI620KS7SMKbu2sQmaf28DSPWZyYGgvcm784LFq4da5nDh6OjjhvKyFpd15a4h7UGcn2QNp4jkpb6zfJCzRARpNb5Ku5PzQ6pMnXLPLD1MxWYJeQOjANoP/VLbguBFSrTsIw6YcHZrFHcNNRDQ4nEJL5OEprJYhccgGNPYrunlf5eN6bjSXHB5ehzTyC3rOsNv0Mdh1y3v',
 	}  
 }
