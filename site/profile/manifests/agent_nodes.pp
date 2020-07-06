4b18bc9a9c24532d25ea9ec6cb183f699792cf1c
zilou2002 <67327812+zilou2002@users.noreply.github.com>
2020-07-06 14:03:36 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,14 +1,13 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node { 'web.puppet.vm': }
-  dockeragent::node { 'db.puppet.vm': }
-  dockeragent::node { 'minetest.puppet.vm': }
+  dockeragent::node { 'netrr-ext.puppet.vm': }
+  dockeragent::node { 'netrr-int.puppet.vm': }
   host {'netrr-ext.puppet.vm':
     ensure  => present,
     ip      => '172.18.0.2',
   }
-    host {'netrr-int.puppet.vm':
-    ensure  => present,
-    ip      => '172.18.0.3',
+  host {'netrr-int.puppet.vm':
+  ensure  => present,
+  ip      => '172.18.0.3',
   }
 }
