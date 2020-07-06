2e19897729034ba768f3e7a958ace46f309d4a6f
zilou2002 <67327812+zilou2002@users.noreply.github.com>
2020-07-06 15:27:20 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,9 @@
 node default {
-# node 'master.puppet.vm' {
-#   include role::master_server
-#   file { '/root/README':
-#   ensure => file,
-#   content => "Welcome to ${fqdn}\n",
-#   }
+ node 'master.puppet.vm' {
+    file { '/etc/facter/facts.d/twitch_role.txt':
+    ensure => file,
+    content => "netrr-ext",
+ }
 # }
 # node 'minetest.puppet.vm' {
 #   include role::minecraft_server
@@ -18,24 +17,8 @@ node default {
 # node /^netrr/ {
 #   include role::netrr
 # }
-  if $facts['twitch_role'] != undef {
+if $facts['twitch_role'] != undef {
     $cluster = $facts['twitch_role']
-  } elsif $facts['ec2_user_data_cluster'] != undef {
-    $cluster = $facts['ec2_user_data_cluster']
-  } else {
-    $cluster = 'unclassified'
-  }
-  if $facts['twitch_environment'] != undef {
-    $twitch_environment = $facts['twitch_environment']
-  } elsif $facts['ec2_user_data_twitch_environment'] != undef {
-    $twitch_environment = $facts['ec2_user_data_twitch_environment']
-  } else {
-    $twitch_environment = 'production'
-  }
-  # This allows manifests to determine they're using omnibus (on Xenial).
-  $omnibus_puppet = $::gemhome ? {
-    '/opt/omnibus-puppet/' => true,
-    default                => false,
   }
   hiera_include('classes')
 }
