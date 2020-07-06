0f0b6a5ba95875e2afb7af314c84f7d293cb8fef
zilou2002 <67327812+zilou2002@users.noreply.github.com>
2020-07-06 16:54:06 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,14 @@
 node default {
 }
+
 node 'master.puppet.vm' {
     include role::master_server
 }
-#node /^netrr/ {
-#   include role::netrr
-#}
-#}
+
+node /^netrr-ext/ {
+   include role::netrr_ext
+}
+
+node /^netrr-int/ {
+   include role::netrr_int
+}
